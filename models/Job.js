89a17b2b51eddec2c0b681ed3c044c74fc5e5af2
progmamun <mamun333089@gmail.com>
2022-10-13 23:15:25 +0600

    job-schema

--- models/Job.js
+++ models/Job.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { ObjectId } = mongoose.Schema.Types;
-const validator = require('validator');
+const validator = require("validator");
 const date = new Date();
 date.setDate(date.getDate() + 5);
 
@@ -9,68 +9,73 @@ const jobSchema = mongoose.Schema(
     createdBy: {
       email: {
         type: String,
-        validate: [validator.isEmail, 'Provide a valid email'],
-        required: [true, 'Please provide a hiring manager email'],
+        validate: [validator.isEmail, "Provide a valid Email"],
+        required: [true, "Please provide a Hiring Manager Email"],
       },
       id: {
         type: ObjectId,
-        ref: 'User',
-        required: [true, 'Please provide a hiring manger id'],
+        ref: "User",
+        required: [true, "Please provide a Hiring Manager id"],
       },
     },
 
     jobTitle: {
       type: String,
-      required: [true, 'Please provide a jobTitle'],
+      required: [true, "Please provide a jobTitle"],
       trim: true,
-      minLength: [3, 'Name must be at least 3 characters.'],
-      maxLength: [200, 'Name is too large'],
+      minLength: [3, "Name must be at least 3 characters."],
+      maxLength: [200, "Name is too large"],
     },
 
     jobType: {
       type: String,
-      required: [true, 'Please provide a jobType'],
+      required: [true, "Please provide a jobType"],
       trim: true,
     },
+
     companyName: {
       type: String,
-      required: [true, 'Please provide a companyName'],
+      required: [true, "Please provide a companyName"],
     },
+
     location: {
       type: String,
-      required: [true, 'Please provide a company location'],
+      required: [true, "Please provide a company location"],
     },
 
     salary: {
       type: Number,
-      required: [true, 'Please provide a salary'],
+      required: [true, "Please provide a salary"],
     },
 
     jobDescription: String,
 
     applicationEmailOrLink: {
       type: String,
-      required: [true, 'Plase provide a applicationEmailOrLink'],
+      required: [true, "Please provide a applicationEmailOrLink"],
       validate: [
         validator.isEmail || validator.isURL,
-        'Provide a valid Email or Link',
+        "Provide a valid Email or Link",
       ],
     },
 
     appliedBy: [
       {
         name: String,
+
         email: {
           type: String,
-          validate: [validator.isEmail, 'Provide a valid Email'],
+          validate: [validator.isEmail, "Provide a valid Email"],
         },
+
         id: {
           type: ObjectId,
-          ref: 'User',
+          ref: "User",
         },
+
         applicationId: {
           type: ObjectId,
-          ref: 'Application',
+          ref: "Application",
         },
 
         date: {
@@ -82,7 +87,7 @@ const jobSchema = mongoose.Schema(
 
     deadline: {
       type: Date,
-      required: [true, 'Please provide a deadline'],
+      required: [true, "Please provide a deadline"],
       default: date,
     },
 
@@ -94,6 +99,6 @@ const jobSchema = mongoose.Schema(
   }
 );
 
-const Job = mongoose.model('Job', jobSchema);
+const Job = mongoose.model("Job", jobSchema);
 
 module.exports = Job;
